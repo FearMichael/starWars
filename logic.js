f29f2c7ecb174d2ce7e01611b21ad830bf13539f
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-01-05 16:19:56 -0500

    initialize files

---
+++ logic.js
@@ -0,0 +1,16 @@
+$(document).ready( function () {
+
+//JSON for each character
+
+//Game start/setup
+
+//instructions to select a good and a bad player, directions change when one or the other is selected
+//Display Health / attack when a character is selected
+
+//attack button attacks with good character and auto counter attacks with bad character
+
+//animation when player is eliminated
+
+
+
+});
